90349238a7653124b6089b58b1758bd19ed5dd96
Amir Daneshkar <amir.daneshkar@hotmail.com>
2022-10-07 00:50:09 +0330

    User service methods added

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -11,17 +11,80 @@ export class UsersService {
 
 	constructor(
 		@InjectRepository(User)
-		private repo: Repository<User>
+		private repo: Repository<User>,
 	) { }
 
 	/**
 	 * Create a new user
+	 * 
 	 * @param dto CreateUserDto
-	 * @returns 
+	 * @returns
 	 */
 	create(dto: CreateUserDto) {
 		const user = this.repo.create(dto);
 
 		return this.repo.save(user);
+	}
+
+	/**
+	 * Find a user based on its Id
+	 * 
+	 * @param id number
+	 * @returns user User
+	 */
+	findOne(id: number) {
+		return this.repo.findOne({
+			where: {
+				id: id,
+			},
+		});
+	}
+
+	/**
+	 * Find users With the given email
+	 * 
+	 * @param email string
+	 * @returns users User[]
+	 */
+	find(email: string) {
+		return this.repo.find({
+			where: {
+				email: email,
+			},
+		});
+	}
+
+	/**
+	 * Update some or whole user attributes 
+	 * after finding them using their ID
+	 * 
+	 * @param id number
+	 * @param attrs User
+	 */
+	async update(id: number, attrs: Partial<User>) {
+		const user = await this.findOne(id);
+
+		if (!user) {
+			throw new Error(`User not found`);
+		}
+
+		Object.assign(user, attrs); // copy and replace all the properties of attrs to user
+
+		return this.repo.save(user);
+	}
+
+	/**
+	 * delete a user with requested ID 
+	 * 
+	 * @param id number
+	 */
+	async remove(id: number) {
+		const user = await this.findOne(id);
+
+		if (!user) {
+			throw new Error(`User not found`);
+		}
+
+		return this.repo.remove(user);
 	}
 }
